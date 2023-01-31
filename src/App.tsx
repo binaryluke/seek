65e4a2545e0fb1729e676e96ccfe2156b9856a8a
Luke Howard <3931305+binaryluke@users.noreply.github.com>
2023-01-31 19:58:22 +1100

    Basic page layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,18 @@
-export const App = () => <h1>Seek | Luke Howard</h1>;
+import * as classes from './App.module.css';
+
+export const App = () => {
+  return <>
+    <header className={classes.header}>
+      <h1>Seek <span>| Luke Howard</span></h1>
+    </header>
+    <main className={classes.main}>
+      <section className={classes.pageTitlePane}>
+        <h2>Checkout</h2>
+      </section>
+    </main>
+    <footer className={classes.footer}>
+      <p>Code test for Seek by Luke Howard</p>
+    </footer>
+  </>;
+};
+
