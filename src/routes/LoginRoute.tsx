a6380d5c08e067bacf1cc03d5652cfc9c3b5dbb7
Luke Howard <3931305+binaryluke@users.noreply.github.com>
2023-02-02 20:47:41 +1100

    Styling for the login route

--- src/routes/LoginRoute.tsx
+++ src/routes/LoginRoute.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Page } from "../components/Page";
 import { CustomerItem, getCustomers } from "../server/mockServer";
+import * as classes from "./LoginRoute.module.css";
 
 export const LoginRoute = () => {
   const [customers, setCustomers] = useState<CustomerItem[]>([]);
@@ -12,15 +13,13 @@ export const LoginRoute = () => {
   }, []);
 
   return (
-    <Page title="Customer Login">
+    <Page className={classes.loginRoute} title="Customer Login">
       <ul>
         {customers.map((customer) => (
           <li key={customer.customerId}>
-            <p>
-              <Link to={`/checkout?customerId=${customer.customerId}`}>
-                {customer.customerName}
-              </Link>
-            </p>
+            <Link to={`/checkout?customerId=${customer.customerId}`}>
+              <p>{customer.customerName}</p>
+            </Link>
           </li>
         ))}
       </ul>
